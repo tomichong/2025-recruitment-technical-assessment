7a7ade886a961b47807e96ad23dfd5a4a793829b
Tomi Chong <z5542085@ad.unsw.edu.au>
2025-02-24 23:48:49 +1100

    Completed backend tasks (python)

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,25 +43,106 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+
+	# remove/convert invalid chars
+	recipeName = re.sub('-', ' ', recipeName)
+	recipeName = re.sub('_', ' ', recipeName)
+	recipeName = re.sub('[^a-zA-Z ]', '', recipeName)
+
+	# handle string with no letters
+	recipeName = recipeName.lower().strip()
+	if not recipeName:
+		return None
+	
+	# convert to list to individual words
+	list = recipeName.split()
+
+	# capitalise words
+	list = [word.capitalize() for word in list]
+
+	# return words with spaces between them
+	return ' '.join(list)
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	entry = request.json
+
+	# incorrect num of keys
+	if len(entry) != 3:
+		return 'invalid', 400
+
+	if 'name' not in entry or 'type' not in entry:
+		return 'no name or type', 400
+	
+	if entry['name'] in cookbook:
+		return 'non unique entry name', 400
+
+	if entry['type'] == 'recipe':
+		if 'requiredItems' not in entry or len(entry['requiredItems']) == 0:
+			return 'no requiredItems', 400
+		
+		for i in entry['requiredItems']:
+			if len(i) != 2:
+				return 'invalid requiredItem', 400
+
+			if 'name' not in i or 'quantity' not in i:
+				return 'invalid keys in requiredItems', 400
+			
+			if i['quantity'] < 0:
+				return 'invalid quantity', 400
+	
+	elif entry['type'] == 'ingredient':
+		if 'cookTime' not in entry:
+			return 'no cookTime', 400
+		
+		if entry['cookTime'] < 0:
+			return 'invalid cookTime', 400
+
+	else:
+		return 'invalid type', 400
+	
+	# delete name from entry and add entry to cookbook
+	cookbook[entry.pop('name')] = entry
+	
+	return '', 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	name = request.args['name']
 
+	if name not in cookbook or cookbook[name]['type'] != 'recipe':
+		return 'invalid name', 400
+
+	ingredient_dict = {'cookTime': 0, 'ingredients': {}}
+	if handle_recipe(name, ingredient_dict) == False:
+		return 'couldnt handle recipe', 400
+
+	result = {'name': name, 'cookTime': ingredient_dict['cookTime'], 'ingredients': []}
+	for ingrName, quantity in ingredient_dict['ingredients'].items():
+		result['ingredients'].append({'name': ingrName, 'quantity': quantity})
+
+	return result, 200
+
+def handle_recipe(name, ingredient_dict):
+	for i in cookbook[name]['requiredItems']:
+		item_name = i['name']
+
+		if item_name not in cookbook:
+			return False
+		
+		if cookbook[item_name] == 'recipe':
+			return handle_recipe(item_name, ingredient_dict)
+		
+		else:
+			ingredient_dict['ingredients'][item_name] = ingredient_dict['ingredients'].get(item_name, 0) + i['quantity']
+			ingredient_dict['cookTime'] += cookbook[item_name]['cookTime'] * i['quantity']
+			return True
 
 # =============================================================================
 # ==== DO NOT TOUCH ===========================================================
